e74a6382ff80cb2df247b18fe23df4ca6bc4f564
Shaheed <shaheedmhmed@gmail.com>
2025-04-16 23:06:11 +0200

    بولبولى

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,60 +5,6 @@ import Typed from 'typed.js';
 import { motion } from 'framer-motion';
 
 const HeroSection = () => {
-<<<<<<< HEAD
-  const el = useRef(null);
-
-  useEffect(() => {
-    const typed = new Typed(el.current, {
-      strings: ['Web Designer', 'Full Stack Developer', 'Creative Thinker'],
-      typeSpeed: 50,
-      backSpeed: 25,
-      backDelay: 2000,
-      loop: true,
-    });
-
-    return () => {
-      typed.destroy();
-    };
-  }, []);
-
-  return (
-    <section className="hero-section" id="home">
-      <motion.div
-        className="content"
-        initial={{ opacity: 0, x: -50 }}
-        animate={{ opacity: 1, x: 0 }}
-        transition={{ duration: 1 }}
-      >
-        <h3>Hey, I'm</h3>
-        <h1>Shaheed Mohamed</h1>
-        <h2>
-          <span ref={el}></span>
-        </h2>
-
-        <div className="buttons">
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://mostaql.com/u/shahid-1"
-            className="btn"
-          >
-            Go to Mostaql
-          </a>
-        </div>
-      </motion.div>
-
-      <motion.div
-        className="image"
-        initial={{ opacity: 0, x: 50 }}
-        animate={{ opacity: 1, x: 0 }}
-        transition={{ duration: 1 }}
-      >
-        <img src={shaheed} alt="Shaheed" />
-      </motion.div>
-    </section>
-  );
-=======
     const el = useRef(null);
 
     useEffect(() => {
@@ -103,7 +49,6 @@ const HeroSection = () => {
             </motion.div>
         </section>
     );
->>>>>>> b383528ac2af14bd756cd1b11dda2d4979f3feca
 };
 
 export default HeroSection;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,16 +2,9 @@ import React, { useState, useEffect } from "react";
 import Navbar from "./NavBar";
 import HeroSection from "./Hero";
 import Projects from "./ProjectHome";
-<<<<<<< HEAD
-import AboutUs from "./AboutUs";
-import ContactUs from "./ContactUs";
-import Skills from "./SkilsHome";
-import ParticlesContainer from './ParticlesContainer';
-=======
 import AboutUs from "./Home/About";
 import ContactUs from "./ContactUs";
 import Skills from "./SkilsHome";
->>>>>>> b383528ac2af14bd756cd1b11dda2d4979f3feca
 import "./css/Home.css";
 
 function Home() {
@@ -32,12 +25,7 @@ function Home() {
                 </div>
             ) : (
                 <>
-<<<<<<< HEAD
-                    
-                    
-=======
                     <Navbar />
->>>>>>> b383528ac2af14bd756cd1b11dda2d4979f3feca
                     <HeroSection />
                     <AboutUs />
                     <Projects />
